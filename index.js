6678e9bbff67df9b178ac0b536ead4699d3796d7
wabwirevalentine <valentine.wabwire@tracom.co.ke>
2025-03-14 08:25:40 +0300

    cretaed my fnc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Follow along with the examples here
+function say(greeting, firstName) {
+  console.log("I was called!");
+  return `${greeting}, ${firstName}!`;
+}
+
+function kyc(fullname, age, location) {
+  console.log("User Fullname: " + fullname);
+  console.log("User Age " + age);
+  console.log("User Location: " + location);
+  console.log(say("Good morning", fullname));
+}
+
+kyc("Valenine", 465, "Nairobi");
